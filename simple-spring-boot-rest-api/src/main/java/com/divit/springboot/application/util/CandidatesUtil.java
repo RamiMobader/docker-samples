f66a0f993353fec11188c4cc1cd186f2fc75ae75
RamiMobader <107487453+RamiMobader@users.noreply.github.com>
2023-11-23 18:39:41 +0200

    Update CandidatesUtil.java

--- simple-spring-boot-rest-api/src/main/java/com/divit/springboot/application/util/CandidatesUtil.java
+++ simple-spring-boot-rest-api/src/main/java/com/divit/springboot/application/util/CandidatesUtil.java
@@ -18,12 +18,9 @@ public final class CandidatesUtil {
 	 */
 	public static List<Candidate> getCandidates() {
 		var candidateList = List.of(
-				new Candidate("John", 2, Set.of("java", "c#", "c++", "golang")),
-				new Candidate("David", 1, Set.of("javascript", "c++")),
-				new Candidate("Diana", 4, Set.of("java", "python")),
-				new Candidate("June", 2, Set.of("ruby", "aws", "docker")),
-				new Candidate("April", 3, Set.of("python", "Erlang", "docker")));
-
+			
+				new Candidate("Rami", 201778, Set.of("python","csharp","c++"))
+			
 		return candidateList;
 	}
 }
